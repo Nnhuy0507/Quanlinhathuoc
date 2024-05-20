593f7a903fdda05fb1e3c1d80761d2b132f4d739
Nnhuy0507 <153533669+Nnhuy0507@users.noreply.github.com>
2024-05-20 21:55:35 +0700

    Add files via upload

---
+++ Quanlynhathuoc/src/main/java/com/mycompany/quanlynhathuoc/Quanlynhathuoc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quanlynhathuoc;
+
+/**
+ *
+ * @author NHU Y
+ */
+public class Quanlynhathuoc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
